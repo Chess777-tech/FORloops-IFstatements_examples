f074fe54c4a1684b52bf34fe5b7319c535090e27
Colby Hessifer <120197361+Chess777-tech@users.noreply.github.com>
2023-03-21 22:05:40 -0500

    Add files via upload

---
+++ for_cal_sumofnums.py
@@ -0,0 +1,12 @@
+"""
+first initialize the varialbe sum to 0
+use the for loop to iterate thru nums 1-10
+in each iteration of the loop we add current num to the sum variable
+
+"""
+
+sum = 0
+for i in range(1, 11):
+    sum += i
+print("Sum of numbers from 1 to 10:", sum)
+
